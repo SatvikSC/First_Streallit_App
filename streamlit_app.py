4fc16213a2e1868c8ad5108d605460727e2dc64d
Satvik Chaudhary <52381290+SatvikSC@users.noreply.github.com>
2022-09-28 15:14:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as stm
 
-stm.title("Streamlit App Title")
+stm.title('My parents New Healty Diner')
+stm.header('Breakfast Menu')
+stm.text('Omega 3 & Blueberry Oatmeal')
+stm.text('Kale, Spinach & Rocket Smoothe\n hard-Boiled Free-Range Egg')
