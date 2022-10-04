57b31d4606100161101b2fd58418cfbbe311cd12
Satvik Chaudhary <52381290+SatvikSC@users.noreply.github.com>
2022-10-04 16:31:21 +0530

    added a user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,10 @@ st.dataframe(fruits_to_show)
 
 # Getting reponse from fruityvice.com
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # st.text(fruityvice_response)
 # st.text(fruityvice_response.json()) # Writting data to the screen
 
