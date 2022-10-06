0ca99d604027aafa695ac1831bce8c35c6c5e9fb
Satvik Chaudhary <52381290+SatvikSC@users.noreply.github.com>
2022-10-06 14:35:23 +0530

    closing the sf connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,7 @@ def get_fruit_load_list():
 if st.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   st.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
@@ -80,4 +81,5 @@ add_my_fruit = st.text_input('What fruit would you like to add?')
 if st.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   st.text(back_from_function)
